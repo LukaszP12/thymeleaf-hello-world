b2240cdb386b41fb16f98affc04cf74e2ca99382
LukaszP <lukaszp4@onet.eu>
2019-09-08 22:59:46 +0200

    This is a new commit.

---
+++ src/main/java/pl/piwowarski/thymeleafhelloworld/CarController.java
@@ -0,0 +1,30 @@
+package pl.piwowarski.thymeleafhelloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    @GetMapping("/car")
+    public String getCar(Model model) {
+
+        Car car1 = new Car("BMW","i8");
+        Car car2 = new Car("Fiat","126");
+        Car car3 = new Car("Polonez","Caro");
+        Car car4 = new Car("Hyundai","i30");
+        List<Car> cars = Arrays.asList(car1,car2,car3,car4);
+        model.addAttribute("cars",cars);
+
+        for(Car car : cars){
+            
+        }
+
+        return "car";
+    }
+
+}
--- src/main/java/pl/piwowarski/thymeleafhelloworld/HelloController.java
+++ src/main/java/pl/piwowarski/thymeleafhelloworld/HelloController.java
@@ -9,9 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String get(Model model) {
-        Car car = new Car("BMW","i8");
         model.addAttribute("name","Przemek");
-        model.addAttribute("car",car);
         return "hello";
     }
 
